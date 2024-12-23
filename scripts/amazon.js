677b783c6ace08ae71b3bff7ff16c9bdfc558285
EldonMusk <konrad.przysowa@onet.pl>
2024-12-23 14:21:54 +0100

    cart-quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -80,8 +80,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       });
     }
 
+    let cartQuantity = 0;
 
-      
-    console.log(cart);
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    })
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
   });
 });
